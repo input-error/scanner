e5f96145adee541c485298b57c8cd0d46b381738
Matthew Mix <matthew.w.mix@gmail.com>
2019-07-23 16:54:37 -0400

    More unit testing.

--- scanner_test.go
+++ scanner_test.go
@@ -2,13 +2,38 @@ package scanner
 
 import "testing"
 
-func TestScan(t *testing.T) {
+func TestScan_valid(t *testing.T) {
 	t.Parallel()
 
-	output, err := Scan("127.0.0.1", "22,631")
+	output, err := Scan("127.0.0.1", []int{22, 631})
 
-	t.Logf("output of scan: %v", output)
 	if err != nil {
 		t.Fatalf("Unknown error occured while executing scan. Error: %s\n", err)
+	}
+
+	t.Logf("output of scan: %v", output)
+}
+
+func TestScan_scanAddress(t *testing.T) {
+	t.Parallel()
+
+	if _, err := Scan("", []int{22, 631}); err == nil {
+		t.Fatal("empty scanAddress should return error")
+	}
+}
+
+func TestScan_ports(t *testing.T) {
+	t.Parallel()
+
+	if _, err := Scan("127.0.0.1", []int{}); err == nil {
+		t.Fatal("empty ports should return error")
+	}
+}
+
+func TestArrayToString(t *testing.T) {
+	expected := "22,631"
+
+	if actual := arrayToString([]int{22, 631}); expected != actual {
+		t.Fatalf("Expected: %s Actual: %s", expected, actual)
 	}
 }
